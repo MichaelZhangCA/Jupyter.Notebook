fcdb4aaf2a776fb8a012c4d3be169ca2090f05ca
Michael Zhang <michael.zh@gmail.com>
2018-02-09 23:10:23 -0500

    cleansed logger with parameter query to avoid special char error

--- Market_Research/core/logger.py
+++ Market_Research/core/logger.py
@@ -4,6 +4,9 @@ class Logger(object):
 
     def __init__(self, servicename, **kwargs):
         self.servicename = servicename
+        self.query = ("INSERT INTO `stock_market`.`ops.operation_log` (`effective_date`, `service_name`, `action_name`, `serverity`, `log_message`) "
+                 "VALUES  ( current_date, '{0}', %s, %s, %s)".format(self.servicename))
+
         return super().__init__(**kwargs)
 
 
@@ -13,31 +16,27 @@ class Logger(object):
     def __exit__(self, exc_type, exc_val, exc_tb):
         pass
 
-    def __get_log_sql(self, serverity, action, msg):
-        query = ("INSERT INTO `stock_market`.`ops.operation_log` (`effective_date`, `service_name`, `action_name`, `serverity`, `log_message`) "
-                 "VALUES  ( current_date, '{0}', '{1}', '{2}', '{3}')".format(self.servicename, action, serverity, msg.replace("'","")))
+    def __get_log_sql(self):
+        
         
         return query
 
-    def __exec_log(self, query):
+    def __exec_log(self, serverity, action, msg):
         with DbConnection() as cnx:
             cur = cnx.cursor()
 
             # clear current record
-            cur.execute(query)
+            cur.execute(self.query, (action, serverity, msg))
             cnx.commit()
             cur.close()
 
 
     def loginfo(self,action,msg):
-        query = self.__get_log_sql("I", action, msg)
-        self.__exec_log(query)
+        self.__exec_log("I", action, msg)
 
 
     def logwarning(self,action,msg):
-        query = self.__get_log_sql("W", action, msg)
-        self.__exec_log(query)
+        self.__exec_log("W", action, msg)
 
     def logerror(self,action,msg):
-        query = self.__get_log_sql("E", action, msg)
-        self.__exec_log(query)
+        self.__exec_log("E", action, msg)
